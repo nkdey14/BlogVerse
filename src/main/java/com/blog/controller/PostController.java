67c83681a4408765a8232b499c2b6285ca7e2464
nkdey14 <im.pandey264@gmail.com>
2024-12-08 20:02:35 +0530

    Enhanced Saving Post Feature with Spring Validation

--- src/main/java/com/blog/controller/PostController.java
+++ src/main/java/com/blog/controller/PostController.java
@@ -3,6 +3,7 @@ package com.blog.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,8 @@ import com.blog.exception.ResourceNotFoundException;
 import com.blog.payload.PostDto;
 import com.blog.service.PostService;
 
+import jakarta.validation.Valid;
+
 @RestController
 @RequestMapping("/api/posts")
 public class PostController {
@@ -23,7 +26,12 @@ public class PostController {
 	// http://localhost:8083/api/posts
 	
 	@PostMapping
-	public ResponseEntity<PostDto> savePostDetails(@RequestBody PostDto postDto) {
+	public ResponseEntity<?> savePostDetails(@Valid @RequestBody PostDto postDto, BindingResult bindingResult) {
+		
+		if(bindingResult.hasErrors()) {
+			
+			return new ResponseEntity<>(bindingResult.getFieldError().getDefaultMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 		
 		PostDto dto = postService.savePost(postDto);
 		
--- src/main/java/com/blog/payload/PostDto.java
+++ src/main/java/com/blog/payload/PostDto.java
@@ -1,17 +1,23 @@
 package com.blog.payload;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
 
 public class PostDto {
 
 	private long id;
 	
+	@NotEmpty
+	@Size(min = 2, message = "Title should be atleast of 2 characters")
 	private String title;
 	
+	@NotEmpty
+	@Size(min = 4, message = "Description should be atleast of 4 characters")
 	private String description;
 	
+	@NotEmpty
+	@Size(min = 4, message = "Content should be atleast of 4 characters")
 	private String content;
 	
 	private String message;
